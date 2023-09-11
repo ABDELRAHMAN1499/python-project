b42f37423e0b2517e3cad8793cdcd9ab884d138b
ABDELRAHMAN1499 <144537620+ABDELRAHMAN1499@users.noreply.github.com>
2023-09-11 17:04:28 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,14 @@
+num1 = float(input (" num1= "))
+operator = input ("operator")
+num2 = float(input (" num2= "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 / num2)
+elif operator == "*":
+    print(num1 * num2)
+    
+    
